85629694b253dd38167351c28cf61e3469d23f75
Adrian Estopace <estopaceadrian@gmail.com>
2024-03-14 21:44:25 +0800

    remove react framework

--- index.php
+++ index.php
@@ -6,14 +6,74 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edusogno</title>
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+
 </head>
 
 <body>
+    <div class="bg-image">
+        <div class="topbar">
+            <img src="./assets/images/Group 181.svg" alt="logo" />
+        </div>
+        <div>
+            <div class="title">Crea il tuo account</div>
+            <div class="App">
+            <form id="myForm">
+                <fieldset>
+                <div class="Field">
+                    <label>Inserisci il nome</label>
+                    <input id="nome" placeholder="Mario" />
+                </div>
+                <div class="Field">
+                    <label>Inserisci il cognome</label>
+                    <input id="cognome" placeholder="Rossi" />
+                </div>
+                <div class="Field">
+                    <label>Inserisci l'email</label>
+                    <input id="email" type="email" placeholder="name@example.com" />
+                </div>
+                <div class="Field">
+                    <label>Inserisci la password</label>
+                    <input id="password" type="password" placeholder="Scrivila qui" />
+                    <div id="togglePassword">
+                        <img src="./assets/images/Vector.svg"  class="eye"/>
+                    </div>
+                </div>
+                <button type="submit">REGISTRATI</button>
+                <div class="forgotpw">
+                    Hai già un account<strong>? Accedi</strong>
+                </div>
+                </fieldset>
+            </form>
+            </div>
 
+        </div>
+        <div class="image-container">
+            <div class="half-circle">
+                <img src="./assets/images/Ellipse 13.png" />
+            </div>
+            <div class="bottomline">
+                <img src="./assets/images/Vector 1.svg" />
+            </div>
+            <div class="darkinnerline">
+                <img src="./assets/images/Vector 5.svg" />
+            </div>
+            <div class="darkbottomline">
+                <img src="./assets/images/Vector 4.svg" />
+            </div>
+            <div class="rocket">
+                <img src="./assets/images/Group 201.png" />
+            </div>
+            <div class="smallcircle">
+                <img src="./assets/images/Ellipse 12.png" />
+            </div>
+        </div>
+    </div>
+    <script src="./assets/js/script.js"></script>
 </body>
 
 </html>
-<?php
+<!-- <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     header("Access-Control-Allow-Origin: *");
@@ -28,17 +88,34 @@
     //echo "test--".$method; die;
     switch($method) {
         case "GET":
-        $alluser = mysqli_query($db_conn, "SELECT * FROM evento");
-        if(mysqli_num_rows($alluser)>0){
-            while($row= mysqli_fetch_array($alluser)){
-                $json_array["userdata"][]=array("id"=>$row['id'], "attendees"=>['attendees'], "eventname"=>['nome_evento'],"eventdate"=>['data_evento']);
+            $alluser = mysqli_query($db_conn, "SELECT * FROM evento");
+            if(mysqli_num_rows($alluser)>0){
+                while($row= mysqli_fetch_array($alluser)){
+                    $json_array["userdata"][]=array("id"=>$row['id'], "attendees"=>$row['attendees'], "eventname"=>$row['nome_evento'],"eventdate"=>$row['data_evento']);
+                }
+                echo json_encode($json_array["userdata"]);
+                return;
+            } else {
+                echo json_encode(["result"=>"please check the data"]);
+                return;
             }
-            echo json_encode($json_array["userdata"]);
-            return;
-        } else {
-            echo json_encode(["result"=>"please check the data"]);
-            return;
-        }
+
+        case "POST":
+            $userpostdata= json_decode(file_get_contents("php://input"));
+            $nome= $userpostdata->nome;
+            $cognome= $userpostdata->cognome;
+            $email= $userpostdata->email;
+            $password= $userpostdata->password;
+            $result= mysqli_query($db_conn, "INSERT INTO utenti (nome, cognome, email, password)
+            VALUES('$nome','$cognome','$email','$password')");
+
+            if($result){
+                echo json_encode(["success"=>"User added successfully"]);
+                return;
+            } else {
+                echo json_encode(["error"=>"Please check user data"]);
+                return;
+            }
         break;
     }
-?>
+?> -->
